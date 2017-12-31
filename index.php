3143f98991255b6e9c51e161a262323b647065b5
DonMagnus <webdesigng@yahoo.com>
2017-12-31 13:21:44 -0500

    Form

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Faultline</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.xx.x/css/uikit.min.css" />
+    </head>
+    
+    
+    <body>
+        
+        <div class="uk-section uk-container">
+            <div class="uk-grid uk-child-width-1-3@s uk-child.width-1-1" uk-grid>
+                <form class="uk-form-stacked js-login">
+                
+                    <div class="uk-margin">
+                        <label class="uk-form-label" for="form-stacked-text">Email</label>
+                        <div class="uk-form-controls">
+                            <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
+                        </div>                    
+                    </div>
+                
+                    <div class="uk-margin">
+                        <label class="uk-form-label" for="form-stacked-text">Password</label>
+                        <div class="uk-form-controls">
+                            <input class="uk-input" id="form-stacked-text" type="Password" required="required" placeholder="Password">
+                        </div>                    
+                    </div>
+                    
+                </form>           
+            </div>
+        </div>
+        
+        
+        
+        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.xx.x/js/uikit.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.xx.x/js/uikit-icons.min.js"></script>
+
+    </body>
+</html>
